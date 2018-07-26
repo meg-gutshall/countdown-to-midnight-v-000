ff6cc7fb588365dff58f62878aa786f51972f1d6
Meghan Gutshall <meghan.gutshall@gmail.com>
2018-07-26 15:21:56 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  def countdown_with_sleep(pause = 1)
+    sleep pause
+  end
+  return "HAPPY NEW YEAR!"
 end
+
